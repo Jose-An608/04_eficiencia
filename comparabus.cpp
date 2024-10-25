5ae063da7488350dba885f5ae12781e4587d1c66
Jose <jvilcanquic@unjbg.edu.pe>
2024-10-25 10:41:14 -0500

    Programa que mide el tiempo de ejecucion de cada algortimo de busqueda

--- comparabus.cpp
+++ comparabus.cpp
@@ -1,171 +1,165 @@
-/*Busqueda binaria*/
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm>
 #include <chrono>
+
 using namespace std;
 
-int busquedaSecuencial(int , int [], int);
-void seleccionDirecta( int [], int);
+int busquedaSecuencial(int, int[], int);
+void metodoShell(int[], int);
 int busquedaBinaria(int, int[], int);
 
-int main(){
-	int m, arr[80000], n, dato, p, dc;
-	srand(time(0));
-	
-	cout << "Digite la cantidad de terminos del arreglo: " << endl;
-	cin >> m;
-	
-	cout << endl;
-	
-	//arreglo generado de numeros
-	
-	for(int i = 0; i<m; i++){
-		arr[i] = rand()% 100000;
-	}
-	
- 	seleccionDirecta(arr, m);
+int main() {
+    int n, p, dc, num_busquedas, m;
 
-	cout << endl;
-	
-	do{
+    const int tam = 1000000; 
+  
+    int* arr = new int[tam];
+    int* arr1 = new int[tam];
+
+    srand(time(0));
+
+    do {
+        cout << "MENU: " << endl;
+        cout << "1. Busqueda secuencial " << endl;
+        cout << "2. Busqueda binaria " << endl;
+        cout << "3. Salir." << endl;
+        cout << "Opcion: ";
+        cin >> n;
 		
-	
-	cout << "Digite el numero que esta buscando: " << endl;
-	cin >> dato;
-	
-	
-	cout << "MENU: " << endl;
-	cout << "1. Busqueda por seleccion " << endl;
-	cout << "2. Busqueda binaria " << endl;
-	cout << "3. Salir." << endl;
-	cout << "Opcion: " << endl;
-	cin >> n;
-	
-	// Variables para medir el tiempo
-	auto start = chrono::high_resolution_clock::now(); 
-	auto end = chrono::high_resolution_clock::now();
-	
-	switch(n){
-		case 1:{
-			
-			
-            const int NUM_BUSQUEDAS = 30000; // Realiza 30,000 búsquedas del mismo dato
-            start = chrono::high_resolution_clock::now(); // Iniciar el tiempo
-
-            for (int i = 0; i < NUM_BUSQUEDAS; i++) {
-                p = busquedaSecuencial(m, arr, dato); // Repetir la búsqueda
-            }
-
-            end = chrono::high_resolution_clock::now(); // Finalizar el tiempo
-            
-            if (p >= 0){
-                cout << "El numero se encuentra en la posicion " << p+1 << " del primer arreglo." << endl;
-            } else {
-                cout << "El numero no se encuentra en el arreglo." << endl;
-            }
-
-            // Calcular la duración
-            chrono::duration<double> duration = end - start;
-            cout << "\nTiempo total de ejecucion para " << NUM_BUSQUEDAS << " búsquedas: " 
-                 << duration.count() << " segundos" << endl;
-        
-        
-        	break;
-		}
-		case 2:{
-			
-			const int NUM_BUSQUEDAS = 30000; // Realiza 30,000 búsquedas del mismo dato
-            start = chrono::high_resolution_clock::now(); // Iniciar el tiempo
-			
-			for (int i = 0; i < NUM_BUSQUEDAS; i++) {
-				p = busquedaBinaria(m, arr, dato);
-			}
-			
-			end = chrono::high_resolution_clock::now(); // Finalizar el tiempo
-			
-			 if (p >= 0) {
-                cout << "El numero se encuentra en la posicion " << p + 1 << " del arreglo." << endl;
-            } else {
-                cout << "El numero no se encuentra en el arreglo." << endl;
-            }
-             // Calcular la duración
-            chrono::duration<double> duration = end - start;
-            cout << "\nTiempo total de ejecucion para " << NUM_BUSQUEDAS << " busquedas: " 
-                 << duration.count() << " segundos" << endl;
-            
-			break;
-		}
+		cout << endl;
 		
-		case 3:
-			cout << "Saliendo..." << endl;
-			break;
-		default: 
-			cout << "Numero incorrecto..." << endl;
-	}
-	
-	cout << endl;
-	
-	cout << "Si desea continuar escriba un numero: (1)SI o (0)NO " << endl;
-	cin >> dc;
-	}while(dc == 1);
-	
-	return 0;
+        cout << "Ingreso al menu, seleccionando la opcion: " << n << endl;
+		
+		cout << endl;
+
+        cout << "Ingrese la cantidad de elementos del arreglo: ";
+        cin >> m;
+		
+		cout << endl;
+		
+        switch (n) {
+            case 1: {
+                cout << "Ingrese el numero de busquedas secuenciales: ";
+                cin >> num_busquedas;
+
+                for (int i = 0; i < m; i++) {
+                    arr1[i] = 1000 + rand() % 100000;
+                }
+
+                cout << "Arreglo generado con exito. Iniciando busqueda..." << endl;
+
+                auto start = chrono::high_resolution_clock::now(); 
+
+                for (int i = 0; i < num_busquedas; i++) {
+                    int dato = arr1[rand() % m];
+                    p = busquedaSecuencial(m, arr1, dato);
+                }
+
+                auto end = chrono::high_resolution_clock::now(); 
+
+                chrono::duration<double> duration = end - start;
+                cout << "Tiempo total para " << num_busquedas << " busquedas secuenciales: "
+                    << duration.count() << " segundos" << endl;
+
+                break;
+            }
+
+            case 2: {
+                cout << "Ingrese el numero de busquedas binarias: ";
+                cin >> num_busquedas;
+
+                for (int i = 0; i < m; i++) {
+                    arr[i] = 1000 + rand() % 100000;
+                }
+
+                metodoShell(arr, m);
+
+                cout << "Arreglo ordenado con exito. Iniciando busqueda binaria..." << endl;
+
+                auto start = chrono::high_resolution_clock::now(); 
+
+                for (int i = 0; i < num_busquedas; i++) {
+                    int dato = arr[rand() % m];
+                    p = busquedaBinaria(m, arr, dato);
+                }
+
+                auto end = chrono::high_resolution_clock::now(); 
+
+                chrono::duration<double> duration = end - start;
+                cout << "Tiempo total para " << num_busquedas << " busquedas binarias: "
+                    << duration.count() << " segundos" << endl;
+
+                break;
+            }
+
+            case 3:
+                cout << "Saliendo..." << endl;
+                break;
+
+            default:
+                cout << "Opcion incorrecta." << endl;
+        }
+
+        cout << "Desea continuar? (1)SI o (0)NO: ";
+        cin >> dc;
+
+    } while (dc == 1);
+
+    // Liberar memoria dinámica
+    delete[] arr;
+    delete[] arr1;
+
+    return 0;
 }
 
-void seleccionDirecta(int arr[], int m){
-	int menor, k;
-	
-	//Algoritmo del metodo de seleccion directa
-	for (int i = 0; i<m-1; i++){
-		menor = arr[i];
-		k = i;
-		for(int j = i+1; j<m; j++){
-			if (menor > arr[j]){
-				menor = arr[j];
-				k = j;
-			}
-		}
-		arr[k] = arr[i];
-		arr[i] = menor;
-	}
-	
+void metodoShell(int arr[], int m) {
+    int menor, k, cen, aux, j;
+
+    // Algoritmo del método de Shell
+    k = m / 2;
+
+    while (k > 0) {
+        cen = 1;
+        while (cen == 1) {
+            cen = 0;
+            j = 0;
+            while (j + k < m) {
+                if (arr[j + k] < arr[j]) {
+                    aux = arr[j];
+                    arr[j] = arr[j + k];
+                    arr[j + k] = aux;
+                    cen = 1;
+                }
+                j++;
+            }
+        }
+        k = k / 2; // Reducir el valor de k
+    }
 }
 
-int busquedaSecuencial(int m, int arr[], int dato){
-	int i = 0, pos;
-	
-	while((i <= m-1) && (arr[i]<dato)){
-		i++;
-	}
-	
-	if ((i>m-1) || (arr[i] > dato)){
-		return pos = -i;
-
-	}else{
-		return pos = i;
-
-	}
-	
+int busquedaSecuencial(int m, int arr1[], int dato) {
+    for (int i = 0; i < m; i++) {
+        if (arr1[i] == dato) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-int busquedaBinaria(int m, int arr[], int dato){
-	int izq = 0, der = m - 1;
+int busquedaBinaria(int m, int arr[], int dato) {
+    int izq = 0, der = m - 1;
 
     while (izq <= der) {
         int n = (izq + der) / 2;
         if (arr[n] == dato) {
-            return n; 
+            return n;
+        } else if (dato > arr[n]) {
+            izq = n + 1;
         } else {
-            if (dato > arr[n]) {
-                izq = n + 1;
-            } else {
-                der = n - 1;
-            }
+            der = n - 1;
         }
     }
-
-    return -1; 
+    return -1;
 }
